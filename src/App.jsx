61b9b93e6af16fb834c13608b19b7430a145aeb0
MSohaib <apple@APPLEs-MacBook-Pro.local>
2026-05-05 18:19:37 +0500

    project created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold text-red-500">
+        Tailwind Test
+      </h1>
+    </div>
+  );
+}
+
+export default App;
